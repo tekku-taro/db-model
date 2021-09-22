94fe9b979383480ad6ef0271cf32737a28b6d922
tekku.taro <tekku.taro@gmail.com>
2021-09-22 15:53:14 +0900

    FileHandlerと設定ファイルの作成。

---
+++ src/Config/Database.php
@@ -0,0 +1,15 @@
+<?php
+namespace Taro\DBModel\Config;
+
+return [
+    'default'=>'mysql',
+    'dbList' => [
+        'mysql'=>[
+            'driver'=>'mysql',
+            'host'=>'localhost',
+            'user'=>'root',
+            'password'=>'',
+            'dbname'=>'tasks',
+        ],
+    ]
+];
---
+++ src/Utilities/FileHandler.php
@@ -0,0 +1,38 @@
+<?php
+namespace Taro\DBModel\Utilities;
+
+define('DS', DIRECTORY_SEPARATOR);
+
+use Taro\DBModel\Exception\FileNotFoundException;
+
+class FileHandler
+{
+    public const CONFIG_PATH = __DIR__ . DS . '..'.DS.'Config'.DS.'Database.php';
+
+    public static function saveAs($filePath, $data): bool
+    {
+        return file_put_contents($filePath, $data);
+    }
+
+    public static function load($filePath): mixed
+    {
+        if(!file_exists($filePath)) {
+            throw new FileNotFoundException($filePath . ' のファイルは存在しません。');
+        }
+
+        return file_get_contents($filePath);
+    }
+
+    public static function loadConfig(): array
+    {
+        if(!file_exists(self::CONFIG_PATH)) {
+            throw new FileNotFoundException(self::CONFIG_PATH . ' の設定ファイルは存在しません。');
+        }
+
+        $configInfo = require(self::CONFIG_PATH);
+        assert(is_array($configInfo));
+        var_dump($configInfo);
+        return $configInfo;
+    }
+
+}
