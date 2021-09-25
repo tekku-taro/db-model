1e84d4bcc7de33da20783ad03f4f18cf73af3527
tekku.taro <tekku.taro@gmail.com>
2021-09-25 15:25:00 +0900

    QueryBuilder クラスの整理。

--- src/Query/QueryBuilder.php
+++ src/Query/QueryBuilder.php
@@ -2,20 +2,11 @@
 namespace Taro\DBModel\Query;
 
 use Taro\DBModel\DB\DB;
-use Taro\DBModel\DB\DbManipulator;
 use Taro\DBModel\Models\Model;
 use Taro\DBModel\Utilities\Paginator;
 
 class QueryBuilder extends BaseBuilder
 {
-    public function __construct(DbManipulator $dbManipulator, $modelName, bool $useBindParam = true)
-    {
-        $this->dbManipulator = $dbManipulator;
-        $this->modelName = $modelName;
-        $this->useBindParam = $useBindParam;
-        $this->query = new Query($dbManipulator, $modelName);
-    }    
-
     public static function query($modelName, bool $useBindParam = true):QueryBuilder
     {
         $dbManipulator = DB::getGlobal()->getManipulator();
@@ -40,28 +31,6 @@ class QueryBuilder extends BaseBuilder
     public function getArrayAll():array    
     {
         return $this->executeAndFetchAll();
-    }
-
-    protected function executeAndFetchAll()
-    {
-        $statement = $this->query->execute();
-        $results = $statement->fetchAll();
-        $statement = null;
-        if($results === false) {
-            return null;
-        }
-        return $results;
-    }
-
-    protected function executeAndFetch()
-    {
-        $statement = $this->query->execute();
-        $results = $statement->fetch();
-        $statement = null;
-        if($results === false) {
-            return null;
-        }
-        return $results;
     }
 
     public function getPaginator(int $number):Paginator    
