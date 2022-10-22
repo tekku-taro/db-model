51d035be9469641dc776b1cc2de894d9f73f8254
tekku.taro <tekku.taro@gmail.com>
2022-10-22 11:51:56 +0900

    設定ファイルとパスの見直し

--- src/Utilities/FileHandler.php
+++ src/Utilities/FileHandler.php
@@ -11,9 +11,7 @@ class FileHandler
 {
     public static $envLoaded = false;
 
-    public const CONFIG_PATH = __DIR__ . DS . '..' . DS .'Config' . DS .'Database.php';
-
-    public const SQLITE_PATH = __DIR__ . DS . '..' . DS . '..' . DS . 'database' . DS .'database.sqlite';
+    public const CONFIG_PATH = __DIR__ . DS . '..' . DS .'Config' . DS .'database.php';
 
     public static function saveAs($filePath, $data): bool
     {
@@ -32,18 +30,36 @@ class FileHandler
     public static function loadConfig(): array
     {
         if(!self::$envLoaded) {
-            $dotenv = Dotenv::createImmutable(__DIR__ . DS . '..' . DS . '..');
+            $dotenv = Dotenv::createImmutable(self::rootPath());
             $dotenv->load();            
         }
 
-        if(!file_exists(self::CONFIG_PATH)) {
-            throw new FileNotFoundException(self::CONFIG_PATH . ' の設定ファイルは存在しません。');
+        if(!file_exists(self::configPath())) {
+            throw new FileNotFoundException(self::configPath() . ' の設定ファイルは存在しません。');
         }
 
-        $configInfo = require(self::CONFIG_PATH);
+        $configInfo = require(self::configPath());
         assert(is_array($configInfo));
         // var_dump($configInfo);
         return $configInfo;
     }
 
+    private static function rootPath()
+    {
+        return dirname(\Composer\Factory::getComposerFile());
+    }
+
+    public static function sqlitePath()
+    {
+        return self::rootPath() . DS . 'database' . DS .'database.sqlite';
+    }
+
+    public static function configPath()
+    {
+        $path = self::rootPath() . DS . 'config' . DS .'database.php';
+        if(!realpath($path)) {
+            $path = self::CONFIG_PATH;
+        }
+        return $path;
+    }
 }
